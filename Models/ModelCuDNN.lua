fa33edf7e0819ca4a2b37137af6575ff8aaceb39
icapalija <ivan.capalija@gmail.com>
2016-03-02 00:40:26 +0100

    CUDNN enabled

--- Models/ModelCuDNN.lua
+++ Models/ModelCuDNN.lua
@@ -1,33 +1,33 @@
 require 'nn'
---require 'cudnn'
+require 'cudnn'
 local model = nn.Sequential()
---cudnn.benchmark = true
---cudnn.fastest = true
---cudnn.verbose = true
+cudnn.benchmark = true
+cudnn.fastest = true
+cudnn.verbose = true
 -- Convolution Layers
 
-model:add(nn.SpatialConvolution(3, 64, 5, 7))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialMaxPooling(2, 2))
+model:add(cudnn.SpatialConvolution(3, 64, 5, 7))
+model:add(cudnn.ReLU(true))
+model:add(cudnn.SpatialMaxPooling(2, 2))
 model:add(nn.Dropout(0.25))
 
-model:add(nn.SpatialConvolution(64, 128, 3, 6))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialMaxPooling(2, 2))
+model:add(cudnn.SpatialConvolution(64, 128, 3, 6))
+model:add(cudnn.ReLU(true))
+model:add(cudnn.SpatialMaxPooling(2, 2))
 model:add(nn.Dropout(0.25))
 
-model:add(nn.SpatialConvolution(128, 256, 3, 5))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialMaxPooling(2, 2))
+model:add(cudnn.SpatialConvolution(128, 256, 3, 5))
+model:add(cudnn.ReLU(true))
+model:add(cudnn.SpatialMaxPooling(2, 2))
 model:add(nn.Dropout(0.25))
 
-model:add(nn.SpatialConvolution(256, 256, 1, 3))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(2, 2))
+model:add(cudnn.SpatialConvolution(256, 256, 1, 3))
+model:add(cudnn.ReLU())
+model:add(cudnn.SpatialMaxPooling(2, 2))
 
-model:add(nn.SpatialConvolution(256, 128, 1, 3))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(2, 3))
+model:add(cudnn.SpatialConvolution(256, 128, 1, 3))
+model:add(cudnn.ReLU())
+model:add(cudnn.SpatialMaxPooling(2, 3))
 model:add(nn.View(128))
 
 
